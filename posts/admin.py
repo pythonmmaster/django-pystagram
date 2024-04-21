2c1f81ae949034583a0bc2a86f82b7e90d6eaffe
minsik <oknamms@naver.com>
2024-04-21 19:45:35 +0900

    feat: django-pystagram 저장

---
+++ posts/admin.py
@@ -0,0 +1,58 @@
+from django.db.models import ManyToManyField
+from django.forms import CheckboxSelectMultiple
+from django.contrib import admin
+from posts.models import Post, PostImage, Comment, HashTag
+import admin_thumbnails
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+    
+@admin_thumbnails.thumbnail('photo')  
+  
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
+
+class LikeUserInline(admin.TabularInline):
+    model = Post.like_users.through
+    verbose_name = '좋아요 한 User'
+    verbose_name_plural = f'{verbose_name} 목록'
+    extra = 1
+    
+    def has_change_permission(self, request, obj=None):
+        return False
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'content',
+    ]
+    inlines = [
+        CommentInline,
+        PostImageInline,
+        LikeUserInline,
+    ]
+    formfield_overrides = {
+        ManyToManyField: {'widget': CheckboxSelectMultiple},
+    }
+    
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'post',
+        'photo',
+    ]
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'post',
+        'content',
+    ]
+    
+@admin.register(HashTag)
+class HashTagAdmin(admin.ModelAdmin):
+    pass
---
+++ posts/views.py
@@ -0,0 +1,108 @@
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponseForbidden
+from django.views.decorators.http import require_POST
+from posts.forms import CommentForm, PostForm
+from django.shortcuts import render, redirect
+from posts.models import Post, Comment, PostImage, HashTag
+
+def feeds(request):
+    if not request.user.is_authenticated:
+        return redirect('users:login')
+    
+    posts = Post.objects.all()
+    comment_form = CommentForm()
+    context = {'posts': posts,
+               'comment_form': comment_form,
+    }
+    
+    return render(request, 'posts/feeds.html', context)
+
+@require_POST
+def comment_add(request):
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.save()
+        
+        if request.GET.get('next'):
+            url_next = request.GET.get('next')
+            
+        else:
+            url_next = reverse('posts:feeds') + f'#post-{comment.post.id}'
+        return HttpResponseRedirect(url_next)
+    
+@require_POST
+def comment_delete(request, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+    if comment.user == request.user:
+        comment.delete()
+        url = reverse('posts:feeds') + f'#post-{comment.post.id}'
+        return HttpResponseRedirect(url)
+    else:
+        return HttpResponseForbidden('이 댓글을 삭제할 권한이 없습니다')
+    
+def post_add(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            
+            for image_file in request.FILES.getlist('images'):
+                PostImage.objects.create(
+                    post=post,
+                    photo=image_file,
+                )
+                
+            tag_string = request.POST.get('tags')
+            if tag_string:
+                tag_names = [tag_name.strip() for tag_name in tag_string.split(',')]
+                for tag_name in tag_names:
+                    tag, _ = HashTag.objects.get_or_create(name=tag_name)
+                    post.tags.add(tag)
+            url = reverse('posts:feeds') + f'#post-{post.id}'
+            return HttpResponseRedirect(url)
+    else:
+        form = PostForm()
+        
+    context = {'form': form}
+    return render(request, 'posts/post_add.html', context)
+
+def tags(request, tag_name):
+    try:
+        tag = HashTag.objects.get(name=tag_name)
+    except HashTag.DoesNotExist:
+        posts = Post.objects.none()
+    else:    
+        posts = Post.objects.filter(tags=tag)
+    
+    context = {
+        'tag_name': tag_name,
+        'posts': posts,
+    }
+    return render(request, 'posts/tags.html', context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    comment_form = CommentForm()
+    context = {
+        'post': post,
+        'comment_form': comment_form,
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+def post_like(request, post_id):
+    post = Post.objects.get(id=post_id)
+    user = request.user
+    
+    if user.like_posts.filter(id=post.id).exists():
+        user.like_posts.remove(post)
+        
+    else:
+        user.like_posts.add(post)
+        
+    url_next = request.GET.get('next') or reverse('posts:feeds') + f'#post-{post.id}'
+    return HttpResponseRedirect(url_next)
